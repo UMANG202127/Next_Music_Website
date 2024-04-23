10216718734096a2c9d3844e7313dab9867a2a88
UMANG202127 <umangnimble2002@gmail.com>
2024-04-23 15:20:30 +0530

    edit some UI

--- src/components/InfiniteMovingCard.tsx
+++ src/components/InfiniteMovingCard.tsx
@@ -4,7 +4,7 @@ import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
 
 function InfiniteMovingCard() {
   return (
-    <div className="">
+    <div className="h-[40rem] w-full dark:bg-black dark:bg-grid-white/[0.2] relative flex flex-col items-center justify-center overflow-hidden">
         <h2 className="text-3xl font-bold text-center mb-8 z-10">Hear Our Harmony: Voices of Success</h2>
         <div className="flex justify-center w-full overflow-hidden px-4 sm:px-6 lg:px-8">
             
