82f02588099dc2662e32222bb75a5c1d5b012efc
UMANG202127 <umangnimble2002@gmail.com>
2024-04-27 14:37:08 +0530

    authentication work goin on

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,8 +2,17 @@
 
 function page() {
   return (
-    <div className="">
-      
+    <div className="min-h-screen bg-black py-12 pt-36">
+      <h1 className="text-lg md:text-5xl text-center 
+        font-sans font-bold mb-8 text-white">Registeration For Admission</h1>
+      <div>
+        <form action="" className="flex flex-wrap justify-center">
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" placeholder="Name"/>
+          <label htmlFor="email">Email Address</label>
+          <input type="email" name="email" placeholder="Email Address"/>
+        </form>
+      </div>
     </div>
   )
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,7 +11,7 @@ function Footer() {
         <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
           <div className="md:flex md:justify-between">
             <div className="mb-6 md:mb-0">
-              <a href="" className="flex items-center">
+              <a href="/" className="flex items-center">
                 <img
                   src="https://flowbite.com/docs/images/logo.svg"
                   className="h-20 me-3"
