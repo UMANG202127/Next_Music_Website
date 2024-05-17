2705d974eb43b550678c2332fa4e614e58cd0f74
UMANG202127 <umangnimble2002@gmail.com>
2024-05-17 16:09:20 +0530

    added profile section

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,13 +1,14 @@
 'use client'
 import axios from 'axios'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import toast from 'react-hot-toast'
 import { useRouter } from 'next/navigation'
 
 
 export default function ProfilePage() {
   const router = useRouter();
+  const [data, setData] = useState("nothing")
 
   const logout = async () =>{
     try {
@@ -20,15 +21,28 @@ export default function ProfilePage() {
       toast.error(error.message)
     }
   }
+
+  const getUserDetails = async () => {
+    const res = await axios.get('api/users/me')
+    console.log(res.data);
+    setData(res.data.data._id)
+    
+  }
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile page</h1>
       <hr />
       <p>profile page</p>
+      <h2 className='p-1 rounded bg-green-500'>{data === "nothing" ? "nothing" : <Link href={`/profile/${data}`}>{data}</Link>}</h2>
+
       <hr />
       <button 
       onClick={logout}
       className="bg-blue-500 hover:bg-blue-700 text-white font bold py-2 px-4 rounded mt-4">logout</button>
+       <button 
+      onClick={getUserDetails}
+      className="bg-purple-900 hover:bg-green-700 text-white font bold py-2 px-4 rounded mt-4">User Details</button>
     </div>
   )
 }
