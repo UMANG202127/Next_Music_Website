9ef241af8916bc17a0a1579ac3ed1741a7eb0098
UMANG202127 <umangnimble2002@gmail.com>
2024-06-27 14:44:22 +0530

    update some changes

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -8,7 +8,7 @@ test('has title', async ({ page }) => {
 });
 
 
-/*test('should navigate to the courses page', async ({ page }) => {
+test('should navigate to the courses page', async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
   await page.goto('http://localhost:3000/')
   // Find an element with the text 'About' and click on it
@@ -61,14 +61,27 @@ test('should navigate to the Home page', async ({ page }) => {
   
 })
 
-*/
+
 
 test('should navigate to the Login page page', async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-  await page.goto('https://musicmaster01.kinde.com/auth/cx/_:nav&m:register&psid:5e2df1128935417db456a28829e85ec7')
+  await page.goto('https://musicmaster01.kinde.com/auth/cx/_:nav&m:register&psid:49105b9492a94031964dc10dedd2e313')
   // Find an element with the text 'About' and click on it
   await page.click('text=Sign in')
   // The new URL should be "/about" (baseURL is used there)
-  await expect(page).toHaveURL('https://musicmaster01.kinde.com/auth/cx/_:nav&m:login&psid:5e2df1128935417db456a28829e85ec7')
+  await expect(page).toHaveURL('https://musicmaster01.kinde.com/auth/cx/_:nav&m:login&psid:49105b9492a94031964dc10dedd2e313')
 
 })
+
+
+test('should navigate to the Register page', async ({ page }) => {
+  // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
+  await page.goto('https://musicmaster01.kinde.com/auth/cx/_:nav&m:login&psid:49105b9492a94031964dc10dedd2e313')
+  // Find an element with the text 'About' and click on it
+  await page.click('text=Create one')
+  // The new URL should be "/about" (baseURL is used there)
+  await expect(page).toHaveURL('https://musicmaster01.kinde.com/auth/cx/_:nav&m:register&psid:49105b9492a94031964dc10dedd2e313')
+
+})
+
+
